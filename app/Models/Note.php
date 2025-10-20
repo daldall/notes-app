b8efdf0935fcc6fc3afd5916cdff71b2b90a5d7e
daldall <afdalrahman459@gmail.com>
2025-10-20 10:48:58 +0700

    first commit

---
+++ app/Models/Note.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'pinned'
+    ];
+
+    protected $casts = [
+        'is_favorite' => 'boolean'
+    ];
+}
